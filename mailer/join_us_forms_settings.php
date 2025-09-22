4dc9cb03ae221158b4f6fc50ce89d1e503f5ef72
spark-nortongauss <innovate@nortongauss.com>
2025-09-22 16:36:21 +0000

    Upload folders

---
+++ mailer/join_us_forms_settings.php
@@ -0,0 +1,8 @@
+<?php
+	/* Forms settings */
+	$subj = "New candidate from 'Norton-Gauss Web'"; //letter subject
+	$to = 'recruitment@nortongauss.com'; // Enter Your E-mail
+	$from = 'noreply@nortongauss.com'; // Admin e-mail
+	$fromName = 'Norton-Gauss Web Recruitment'; // Your company name
+	$charset = 'UTF-8';
+?>

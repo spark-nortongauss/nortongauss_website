f02588e9f6e3452759cbbd8bd0980bc4c39f2b2c
spark-nortongauss <innovate@nortongauss.com>
2025-09-23 05:36:08 +0000

    delete all files

--- mailer/forms_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/* Forms settings */
-	$subj = "New message from 'Norton-Gauss Web'"; //letter subject
-	$to = 'sales@nortongauss.com'; // Enter Your E-mail
-	$from = 'noreply@nortongauss.com'; // Admin e-mail
-	$fromName = 'Norton-Gauss Web'; // Your company name
-	$charset = 'UTF-8';
-?>
--- mailer/join_us_forms_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/* Forms settings */
-	$subj = "New candidate from 'Norton-Gauss Web'"; //letter subject
-	$to = 'recruitment@nortongauss.com'; // Enter Your E-mail
-	$from = 'noreply@nortongauss.com'; // Admin e-mail
-	$fromName = 'Norton-Gauss Web Recruitment'; // Your company name
-	$charset = 'UTF-8';
-?>
